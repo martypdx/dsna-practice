bc7b23ed19dadca5c77f3d14edd51e6ea812925d
Marty Nelson <marty@martypdx.com>
2022-06-09 11:23:45 -0700

    objects practice

--- objects/objects.js
+++ objects/objects.js
@@ -1,4 +1,26 @@
 
-export function makePet() {
+export function makePet(name, type, age, food, toys) {
+    return { name, type, age, food, toys };
+}
 
+export function formatWork({ book, author }) {
+    return {
+        work: book.title,
+        writer: author.first + ' ' + author.last,
+    };
+}
+
+export function formatRevenge({ writer, work }) {
+    // const { writer, work } = data;
+    // const writer = data.writer;
+    // const work = data.work;
+
+    const [first, last] = writer.split(' ');
+
+    return {
+        author: { first, last },
+        book: {
+            title: work
+        }
+    };
 }
